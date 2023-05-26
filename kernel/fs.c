03154da76cb084831d8250502effa734c4b31a2b
Fennel1 <627235787@qq.com>
2023-05-26 15:08:45 +0000

    fs

--- kernel/fs.c
+++ kernel/fs.c
@@ -391,6 +391,7 @@ bmap(struct inode *ip, uint bn)
     // Load indirect block, allocating if necessary.
     if((addr = ip->addrs[NDIRECT]) == 0)
       ip->addrs[NDIRECT] = addr = balloc(ip->dev);
+      
     bp = bread(ip->dev, addr);
     a = (uint*)bp->data;
     if((addr = a[bn]) == 0){
@@ -408,6 +409,7 @@ bmap(struct inode *ip, uint bn)
 
     if((addr = ip->addrs[NDIRECT+1]) == 0)
       ip->addrs[NDIRECT+1] = addr = balloc(ip->dev);
+
     bp = bread(ip->dev, addr);
     a = (uint*)bp->data;
     if ((addr = a[idx_1]) == 0){
